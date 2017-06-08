114452ebbb47a3dd653e5ede64683e9ef3ef5e01
Dan Nelson <dnelson@cybertooth.ca>
2017-06-07 23:13:00 -0700

    Now deploying to the staging environment.

--- config/deploy.js
+++ config/deploy.js
@@ -2,24 +2,23 @@
 
 module.exports = function(deployTarget) {
   var ENV = {
-    build: {}
+    build: {
+      environment: 'production'
+    },
+    s3: {
+      bucket: 'staging.improvementassociates.com',
+      filePattern: '**/*.{js,css,png,gif,ico,jpg,jpg-c200,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,html}',
+      profile: 'improvementassociates',
+      region: 'ca-central-1'
+    }
     // include other plugin configuration that applies to all deploy targets here
   };
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
 
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
-
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
-  }
+  // if (deployTarget === 'production') {
+  //   ENV.build.environment = 'production';
+  //   // configure other plugins for production deploy target here
+  // }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
