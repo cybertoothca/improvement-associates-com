8aca6adb0babcccd9134b847efd2f8984a585e8e
Dan Nelson <dnelson@cybertooth.ca>
2017-06-10 14:17:16 -0700

    Document title and testing in chrome

--- testem.js
+++ testem.js
@@ -3,10 +3,13 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+    "Chrome"
   ],
   "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
-  ]
+    "Chrome",
+    "Firefox"
+  ],
+  "browser_args": {
+    "Chrome": ["--headless", "--remote-debugging-port=9222"]
+  }
 };
