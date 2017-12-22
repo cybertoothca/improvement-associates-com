f66503ca0f04c92c7f9a0982c20444903ac0aec5
Dan Nelson <dnelson@cybertooth.ca>
2017-12-22 10:52:48 -0700

    Deploy configuration changes for the production website.

--- config/deploy.js
+++ config/deploy.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 
-module.exports = function (/*deployTarget*/) {
+module.exports = function (deployTarget) {
   var ENV = {
     build: {
       environment: 'production'
@@ -9,20 +9,22 @@ module.exports = function (/*deployTarget*/) {
       bucket: 'cdn.improvementassociates.com',
       filePattern: '**/*.{js,css,png,gif,ico,jpg,jpg-c200,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf}',
       profile: 'improvementassociates',
-      region: 'ca-central-1'
+      region: 'us-west-2'
     },
     's3-index': {
       bucket: 'staging.improvementassociates.com',
       profile: 'improvementassociates',
       region: 'ca-central-1'
     }
-    // include other plugin configuration that applies to all deploy targets here
   };
 
-  // if (deployTarget === 'production') {
-  //   ENV.build.environment = 'production';
-  //   // configure other plugins for production deploy target here
-  // }
+  if (deployTarget === 'production') {
+    ENV['s3-index'] = {
+      bucket: 'improvementassociates.com',
+      profile: 'improvementassociates',
+      region: 'ca-central-1'
+    }
+  }
 
   // Note: if you need to build some configuration asynchronously, you can return
   // a promise that resolves with the ENV object instead of returning the
